6b2aed4257b46f6072081f231daaa324d828d7fd
Charles Carrier <charles.e.carrier@gmail.com>
2018-11-02 16:46:08 +0800

    vijay form updates

--- src/app/tests-create/tests-create.component.ts
+++ src/app/tests-create/tests-create.component.ts
@@ -35,6 +35,8 @@ export class TestsCreateComponent implements OnInit {
     });
   }
 
+// Each form is only 1 level deep
+
   get examType() {
     return this.examForm.get('examType');
   }
@@ -43,10 +45,27 @@ export class TestsCreateComponent implements OnInit {
     return this.examForm.get('name');
   }
 
+  // control is a variable name,
+  // here control is converting this.examForm.controls.section into type FormArray
+  // even though we define sections above, we need to explicitly tell angualar it is of type
+  // form array.
+  // form controls. this.examForm.controls.section- controls contains all the key value form that are 
+  // part of exam form. Note that we are not starting FormControls because we are using
+  // form builder. Everything inside formbuilder is actually form control
+  // the sections is part of a formbuilder group therefore we need to use .controls to access it.
+
+  // the 1st layer is the sections.
+  // the 2nd layer is the question.
+  // the 3rd layer is the answer choice.
+
+  //
+
   addNewSection() {
-    const control = <FormArray>this.examForm.controls.sections;
+    const control = <FormArray> this.examForm.controls.sections;
     control.push(
       this.fb.group({
+        // note that here question could be a string but we make it an array so we 
+        // can pass in validations as another value
         sectionType: ['', Validators.required],
         questions: this.fb.array([])
       })
@@ -58,18 +77,33 @@ export class TestsCreateComponent implements OnInit {
     control.removeAt(index);
   }
 
+  // note that this control is something we access from the component
+  // when we use addNewQuestion in the html we pass in which section this question belongs to
+  // so it knows what array to push these questions into.
   addNewQuestion(control) {
     control.push(
       this.fb.group({
+        // note that here answer could be a string but we make it an array so we can pass in validations as another value
         answer: ['', Validators.required]
       })
     );
   }
 
+  // control defines which section we are looking at
   deleteQuestion(control, index) {
     control.removeAt(index);
   }
 
+  // this.examForm.value, with value being the content of the exam form, this is of type js Object.
+  // we need to convert it into a JSON to submit it
+  // so that is what jsonValue does.
+  // this.api.postTest talks to the service API for sending the jsonValues
+  //
+    // postTest(exam): Observable<any> {
+    //   return this.http.post(apiUrl, exam, httpOptions);
+    // }
+    // apiUrl is defined in the service & it is the endpoint url we want to post the data to.
+    // httpOptions just defines the json.
   submitHandler() {
     const rawValue = this.examForm.value;
     const jsonValue = JSON.stringify(rawValue);
