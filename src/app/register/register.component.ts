62f5da5c8d7b59b7a09a9919df76ed3a8dde7d6d
Charles Carrier <charles.e.carrier@gmail.com>
2019-01-15 14:59:23 +0800

    setting up the formgroup for register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../authentication.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -9,26 +11,61 @@ import { Observable } from 'rxjs';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  user = {
-    name: '',
-    email: '',
-    password: '',
-    isAdmin: false
-  };
-
-  constructor(private auth: AuthenticationService, private router: Router) { }
+  userForm: FormGroup;
+  // user = {
+  //   name: '',
+  //   email: '',
+  //   password: '',
+  //   isAdmin: false
+  // };
+  success = false;
+  loading = false;
+  constructor(private fb: FormBuilder, private auth: AuthenticationService, private router: Router) { }
 
   ngOnInit() {
-  }
-
-  onSubmit() {
-    const userJSON = JSON.stringify(this.user);
-    this.auth.registerUser(userJSON).subscribe(result => {
-      console.log(this.auth.isLoggedIn());
-      this.router.navigate(['/tests']);
-    }, err => {
-      console.log(err);
+    this.userForm = this.fb.group({
+      userName: ['', Validators.required],
+      userEmail: ['', Validators.required],
+      userPassword: ['', Validators.required],
     });
   }
+
+  get userName() {
+    return this.userForm.get('userName');
+  }
+
+  get userEmail() {
+    return this.userForm.get('userEmail');
+  }
+
+  get userPassword() {
+    return this.userForm.get('userPassword');
+  }
+
+
+  // submitHandler() {
+  //   this.loading = true;
+  //   const rawValue = this.userForm.value;
+  //   const jsonValue = JSON.stringify(rawValue);
+  //   this.api.postTest(jsonValue).subscribe((result) => {
+  //     this.loading = false;
+  //     this.success = true;
+  //     this.dialog.open(SuccessDialogComponent);
+  //     this.router.navigate(['/tests']);
+  //     console.log(result);
+  //   }, (error) => {
+  //     this.loading = false;
+  //   });
+  // }
+
+  // onSubmit() {
+  //   const userJSON = JSON.stringify(this.user);
+  //   this.auth.registerUser(userJSON).subscribe(result => {
+  //     console.log(this.auth.isLoggedIn());
+  //     this.router.navigate(['/tests']);
+  //   }, err => {
+  //     console.log(err);
+  //   });
+  // }
 
 }
